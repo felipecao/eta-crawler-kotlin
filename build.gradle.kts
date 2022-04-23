ca7ece8dbf70fa1f9fb9cf43685456edcec467ca
Felipe Carvalho <felipe.carvalho@gmail.com>
2022-04-23 17:33:54 +0200

    Trying to make it work...

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 group = "pe.morena.work"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_12
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
@@ -36,7 +36,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "12"
+		jvmTarget = "11"
 	}
 }
 
